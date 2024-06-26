a18ac1ca84ba927c3567f484c87f263c6279a9ba
Vasu2702 <100480180+Vasu2702@users.noreply.github.com>
2024-06-26 12:05:58 +0530

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,30 @@
+// Filename: ./components/ToggleSwitch.js
+
+import React, { useState} from "react";
+import "./Toggle.css";
+
+const Toggle = ({ label, onToggleChange }) => {
+    const [isChecked, setIsChecked] = useState(false);
+  
+    const handleChange = () => {
+      setIsChecked(!isChecked);
+      onToggleChange(!isChecked); // Passes the new state of the toggle
+    };
+return (
+	<div className="container">
+<h3>{label}</h3>
+	<div className="toggle-switch">
+   
+		<input type="checkbox" className="checkbox"
+			name={label} id={label} checked={isChecked}
+            onChange={handleChange}/>
+		<label className="label" htmlFor={label}>
+		<span className="inner" />
+		<span className="switch" />
+		</label>
+	</div>
+	</div>
+);
+};
+
+export default Toggle;
